1ca8c53d783d5c2d9eade7a208d229d4899a0ef3
The Westminster Wiredcats (FRC 2415) <wiredcats2415@gmail.com>
2012-12-10 21:27:47 -0500

    Edited .gitignore to avoid future derp

--- src/JavaWiredcats2012.java
+++ src/JavaWiredcats2012.java
@@ -1,29 +1,18 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+/**
+ * Main class for the Java version of Team 2415's 2012 C++ code
+ * 
+ * @author lillychin
+ */
 
 import edu.wpi.first.wpilibj.*;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the SimpleRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
 public class JavaWiredcats2012 extends SimpleRobot {
 
     Victor vicLeft, vicRight;
-    Joystick joy1;
 
     public JavaWiredcats2012() {
         vicLeft = new Victor(1);
         vicRight = new Victor(2);
-
-        joy1 = new Joystick(1);
     }
 
     /**
@@ -38,8 +27,8 @@ public class JavaWiredcats2012 extends SimpleRobot {
      */
     public void operatorControl() {
         while(isOperatorControl()){
-            vicLeft.set(joy1.getRawAxis(2));
-            vicRight.set(joy1.getRawAxis(5));
+            vicLeft.set(Joystick2415.primaryGetLeftY());
+            vicRight.set(Joystick2415.primaryGetRightY());
         }
     }
 }
