5cdbc1f8e768e2cd6af5f2ba4e688f4c75ddaa6e
The Westminster Wiredcats (FRC 2415) <wiredcats2415@gmail.com>
2012-12-23 23:37:16 -0500

    Edited Sam Crane's CSV - comments on bugs

--- src/Util2415/CSVReader.java
+++ src/Util2415/CSVReader.java
@@ -4,36 +4,32 @@ import java.util.Hashtable;
 
 /**
  * Code to read from CSV file and reupdate constants
- * 
+ *
  * @author SamCrane
  */
+public abstract class CSVReader {
 
-public abstract class CSVReader 
-{
-	
-	private static Hashtable<String, Double> resources = new Hashtable<String, Double>();
-	
-	public static double getValue(String s)
-	{
-		return resources.get(s);
-	}
-	
-	public void getFromFile(String s)
-	{
-		try
-		{
-			Scanner sc = new Scanner(s);
-			
-			while (sc.hasNext())
-			{
-				resources.put(sc.next(), Double.parseDouble(sc.next()) );
-			}
-			sc.close();
-		}
-		catch (Exception ioe)
-		{
-			ioe.printStackTrace();
-			System.exit(-1);
-		}
-	}
+    //Version 1.3 does not support generics :( :( :(
+    //Also, they use a sort of pidgin Hashtable which works slightly differently
+    // http://docs.oracle.com/javame/config/cldc/ref-impl/cldc1.1/jsr139/index.html
+    
+    private static Hashtable<String, Double> resources = new Hashtable<String, Double>();
+
+    public static double getValue(String s) {
+        return resources.get(s);
+    }
+
+    public void getFromFile(String s) {
+        try {
+            Scanner sc = new Scanner(s);
+
+            while (sc.hasNext()) {
+                resources.put(sc.next(), Double.parseDouble(sc.next()));
+            }
+            sc.close();
+        } catch (Exception ioe) {
+            ioe.printStackTrace();
+            System.exit(-1);
+        }
+    }
 }
--- src/Util2415/Scanner.java
+++ src/Util2415/Scanner.java
@@ -1,75 +1,68 @@
 package Util2415;
 
+import java.io.InputStream;
+
 /**
  * Homebrew Scanner class
- * 
+ *
  * @author SamCrane
  */
+public class Scanner {
 
-public class Scanner 
-{
-	
-	public static final char END_CHARACTER = '#';
-	private boolean endOfFile = false;
-	InputStream input;
-	
-	public Scanner(String fileName)
-	{
-		try
-		{
-			InputConnection connection = (InputConnection) Connector.open("file:///" + fileName, Connector.READ);
-			input = connection.openDataInputStream();
-		}
-		catch (Exception e)
-		{
-			System.out.println("File could not be opened. Most likely file " s + " was misplaced.");
+    public static final char END_CHARACTER = '#';
+    private boolean endOfFile = false;
+    InputStream input;
+
+    public Scanner(String fileName) {
+        try {
+            //InputConnection and Connector are not declared. Having trouble finding what libraries I should import
+            InputConnection connection = (InputConnection) Connector.open("file:///" + fileName, Connector.READ);
+            input = connection.openDataInputStream();
+        } catch (Exception e) {
+            //s was not declared, so I changed it to fileName
+            System.out.println("File could not be opened. Most likely file " + fileName + " was misplaced."
+            );
 			e.printStackTrace();
-			System.exit(-1);
-		}
-	}
+            System.exit(-1);
+        }
+    }
 
-	
-	/* returns the next word in
-	 * the stream of data. A quirk to this
-	 * method is that if it finds the specified
-	 * "halt" value, '#'
-	 */
-	public String next()
-	{
-		String s = "";
-		char read = input.read();
-		while (read == ' ')
-		{
-			read = input.read();
-			if (read == END_CHARACTER)
-			{
-				endOfFile = true;
-				return s; //if the final character is found, then return this.
-			}
-		}
-		while (read != ' ')
-		{
-			s += read;
-			read = input.read();
-			if (read == END_CHARACTER)
-				{
-					endOfFile = true;
-					return s; //if the final character is found, then return this.
-				}
-		}
-		return s;
-	}
-	
-	public boolean hasNext()
-	{
-		return !endOfFile; //they are logical opposites.
-						   //the last piece of information will end
-						   //end in '#', to notify end of file.
-	}
-	
-	
-	public void close()
-	{
-		input.close();
-	}
+    /* returns the next word in
+     * the stream of data. A quirk to this
+     * method is that if it finds the specified
+     * "halt" value, '#'
+     */
+    public String next() {
+        String s = "";
+        //Unhappy with having input.read() return an int but then have "read" being a char
+        //Tried casting it but then it was unhappy with uncaught IOExceptions
+        char read = input.read();
+        while (read == ' ') {
+            read = input.read();
+            if (read == END_CHARACTER) {
+                endOfFile = true;
+                return s; //if the final character is found, then return this.
+            }
+        }
+        while (read != ' ') {
+            s += read;
+            read = input.read();
+            if (read == END_CHARACTER) {
+                endOfFile = true;
+                return s; //if the final character is found, then return this.
+            }
+        }
+        return s;
+    }
+
+    public boolean hasNext() {
+        return !endOfFile; //they are logical opposites
+                           //the last piece of information will end
+                           //end in '#', to notify end of file.
+    }
+
+    public void close() {
+        //Uncaught IO exception
+        input.close();
+    }
 }
