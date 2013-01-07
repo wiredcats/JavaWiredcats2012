7c7b2e571532e04e4299f65586b6c0145498e252
wiredCatBruceCrane <samcrane@westminster.net>
2013-01-07 16:20:44 -0500

    Update src/Util2415/Scanner.java

--- src/Util2415/Scanner.java
+++ src/Util2415/Scanner.java
@@ -3,6 +3,7 @@ package Util2415;
 import java.io.InputStream;
 import javax.microedition.io.InputConnection;
 import javax.microedition.io.Connector;
+import java.io.IOException;
 
 /**
  * Homebrew Scanner class
@@ -19,7 +20,7 @@ public class Scanner {
         try {
             InputConnection connection = (InputConnection) Connector.open("file:///" + fileName, Connector.READ);
             input = connection.openDataInputStream();
-        } catch (Exception e) {
+        } catch (IOException e) {
             //s was not declared, so I changed it to fileName
             System.out.println("File could not be opened. Most likely file " + fileName + " was misplaced."
             );
@@ -37,9 +38,13 @@ public class Scanner {
         String s = "";
         //Unhappy with having input.read() return an int but then have "read" being a char
         //Tried casting it but then it was unhappy with uncaught IOExceptions
-        char read = input.read();
+        
+        try
+        {
+        	
+        char read = (char)input.read();
         while (read == ' ') {
-            read = input.read();
+            read = (char)input.read();
             if (read == END_CHARACTER) {
                 endOfFile = true;
                 return s; //if the final character is found, then return this.
@@ -47,11 +52,19 @@ public class Scanner {
         }
         while (read != ' ') {
             s += read;
-            read = input.read();
+            read = (char)input.read();
             if (read == END_CHARACTER) {
                 endOfFile = true;
                 return s; //if the final character is found, then return this.
             }
+        }
+        
+        }
+        catch (IOException ioe)
+        {
+        	ioe.printStackTrace();
+        	System.out.println("IOException: the file is not found");
+        	System.exit(-1);
         }
         return s;
     }
@@ -64,6 +77,14 @@ public class Scanner {
 
     public void close() {
         //Uncaught IO exception
+        try
+        {
         input.close();
+        }
+        catch (IOException ioe)
+        {
+        	ioe.printStackTrace();
+        	System.out.println("IOException: It couldn't close the file, which I personally do not understand");
+        }
     }
 }
