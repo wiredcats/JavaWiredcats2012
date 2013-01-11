2c37db7fe1c3de6407c99d85164085f6a49d2e73
The Westminster Wiredcats (FRC 2415) <wiredcats2415@gmail.com>
2013-01-11 09:32:02 -0500

    Update src/Util2415/Scanner.java

--- src/Util2415/Scanner.java
+++ src/Util2415/Scanner.java
@@ -19,12 +19,13 @@ public class Scanner {
     public Scanner(String fileName) {
         try {
             InputConnection connection = (InputConnection) Connector.open("file:///" + fileName, Connector.READ);
+            System.out.println("Lilly wuz here");
             input = connection.openDataInputStream();
+            System.out.println("Maybe not here");
         } catch (IOException e) {
             //s was not declared, so I changed it to fileName
-            System.out.println("File could not be opened. Most likely file " + fileName + " was misplaced."
-            );
-			e.printStackTrace();
+            System.out.println("Hi, this is Sam Crane with the error report:");
+            e.printStackTrace();
             System.exit(-1);
         }
     }
@@ -36,55 +37,55 @@ public class Scanner {
      */
     public String next() {
         String s = "";
-        //Unhappy with having input.read() return an int but then have "read" being a char
-        //Tried casting it but then it was unhappy with uncaught IOExceptions
-        
-        try
-        {
-        	
-        char read = (char)input.read();
-        while (read == ' ') {
-            read = (char)input.read();
-            if (read == END_CHARACTER) {
-                endOfFile = true;
-                return s; //if the final character is found, then return this.
+
+        try {
+            char read = (char) input.read();
+            System.out.println(read);
+            while (read == ' ' || read == '\n') {
+                read = (char) input.read();
+                System.out.println(read + "~~~");
+                if (read == END_CHARACTER) {
+                    endOfFile = true;
+                    System.out.println(s);
+                    System.out.println("0---");
+                    return s; //if the final character is found, then return this.
+                }
             }
-        }
-        while (read != ' ') {
-            s += read;
-            read = (char)input.read();
-            if (read == END_CHARACTER) {
-                endOfFile = true;
-                return s; //if the final character is found, then return this.
+            while (read != ' ' && read != ',' && read != '\n') {
+                s += read;
+                System.out.println(s + "...");
+                read = (char) input.read();
+                System.out.println(read);
+                if (read == END_CHARACTER) {
+                    endOfFile = true;
+                    System.out.println(s);
+                    System.out.println("1---");
+                    return s; //if the final character is found, then return this.
+                }
             }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            System.out.println("IOException: the file is not found");
+            System.exit(-1);
         }
-        
-        }
-        catch (IOException ioe)
-        {
-        	ioe.printStackTrace();
-        	System.out.println("IOException: the file is not found");
-        	System.exit(-1);
-        }
+        System.out.println(s);
+        System.out.println("2---");
         return s;
     }
 
     public boolean hasNext() {
         return !endOfFile; //they are logical opposites
-                           //the last piece of information will end
-                           //end in '#', to notify end of file.
+        //the last piece of information will end
+        //end in '#', to notify end of file.
     }
 
     public void close() {
         //Uncaught IO exception
-        try
-        {
-        input.close();
-        }
-        catch (IOException ioe)
-        {
-        	ioe.printStackTrace();
-        	System.out.println("IOException: It couldn't close the file, which I personally do not understand");
+        try {
+            input.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            System.out.println("IOException: It couldn't close the file, which I personally do not understand");
         }
     }
 }
